4843240d6d37ca8070aa9ce4fcc926b7a0de01a4
Sunoj Jose <l00162972@student.lyit.ie>
2021-08-27 21:51:54 +0100

    Update Main.java
    update contains java-8 compatible code

--- src/jose/sunoj/Main.java
+++ src/jose/sunoj/Main.java
@@ -3,13 +3,13 @@ import javax.swing.JOptionPane;
 public class Main {
 
 	public static void main(String[] args) {
-		var name = JOptionPane.showInputDialog("Enter a Name.");
+		String name = JOptionPane.showInputDialog("Enter a Name.");
 		JOptionPane.showMessageDialog(null, "Hello" + name);
 		
-		var age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age."));
+		int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age."));
 		JOptionPane.showMessageDialog(null, "you are " + age + " years old.");
 		
-		var height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height."));
+		double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height."));
 		JOptionPane.showMessageDialog(null, "you are " + height + " cm tall.");
 
 	}
